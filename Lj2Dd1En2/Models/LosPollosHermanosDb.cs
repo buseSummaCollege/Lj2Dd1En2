8ae40a6b5b56fd6a8a781dcb2e9d77ac93dae2ce
buseSummaCollege <buse@summacollege.nl>
2022-03-02 13:08:50 +0100

    MySql package geinstalleerd AppConfig toegevoegd Databaseclass Gemaakt met Connectionstring

---
+++ Lj2Dd1En2/Models/LosPollosHermanosDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace Lj2Dd1En2.Models
+{
+    public class LosPollosHermanosDb
+    {
+        private readonly string connString = ConfigurationManager.ConnectionStrings["Lj2Dd1En2Conn"].ConnectionString;
+    }
+}
