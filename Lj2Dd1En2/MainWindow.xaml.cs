a6d434c575225ef6b3e39be17bebe959c920c96c
buseSummaCollege <buse@summacollege.nl>
2022-03-02 10:21:08 +0100

    Startcode en sql script opdracht 1

--- Lj2Dd1En2/MainWindow.xaml.cs
+++ Lj2Dd1En2/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+using System.IO;
+
 namespace Lj2Dd1En2
 {
     /// <summary>
@@ -24,5 +27,71 @@ namespace Lj2Dd1En2
         {
             InitializeComponent();
         }
+
+
+        private void LbAutos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAfbeelding1_Click(object sender, RoutedEventArgs e)
+        {
+            byte[]? afbeelding = GetLocalPicture();
+            if (afbeelding != null)
+            {
+                imgAfbeelding1.Source =
+                    new ImageSourceConverter().ConvertFrom(afbeelding) as ImageSource;
+            }
+        }
+
+        private void BtnAfbeelding2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region GetLocalPicture
+        // GetLocalPicture leest een afbeelding op je computer in een array van byte in.
+        // GetLocalPicture heef de volgende waarden:
+        // - null: geen afbeelding ingelezen
+        // - ongelijk null: de ingelezen afbeelding
+        private byte[]? GetLocalPicture()
+        {
+            // Create OpenFileDialog 
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            dlg.FilterIndex = 3;    // 3de blok, is jpg
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                return File.ReadAllBytes(dlg.FileName);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        #endregion
     }
 }
