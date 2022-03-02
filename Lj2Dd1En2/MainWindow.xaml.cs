610967f5ea268f2895ee2dcc834b4e0dca10a003
buseSummaCollege <buse@summacollege.nl>
2022-03-02 10:49:44 +0100

    Voorbeelduitwerking

--- Lj2Dd1En2/MainWindow.xaml.cs
+++ Lj2Dd1En2/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +16,7 @@ using System.Windows.Shapes;
 using System.Data;
 using Microsoft.Win32;
 using System.IO;
+using Lj2Dd1En2.Models;
 
 namespace Lj2Dd1En2
 {
@@ -25,172 +25,172 @@ namespace Lj2Dd1En2
     /// </summary>
     public partial class MainWindow : Window
     {
-        MySqlConnection MonteurVanAuto = new("server=localhost;database=carsdb;uid=root;pwd=;");
-        byte[]? nieuw;
-        byte[]? wijzig;
+        #region private fields
+        private byte[]? afbeeldingNieuweAuto;
+        private byte[]? afbeeldingBestaandeAuto;
+        private readonly CarsDb db = new();
+        #endregion
 
         public MainWindow()
         {
             InitializeComponent();
-            lezen();
+            PopulateLbCars();
         }
 
-
-        private void lezen()
+        // Method zet alle auto's uit de database in de ListBox lbCars.
+        private void PopulateLbCars()
         {
-            DataTable t = new();
-            MonteurVanAuto.Open();
-            MySqlCommand hulp = MonteurVanAuto.CreateCommand();
-            hulp.CommandText = "select * from cars order by make;";
-            t.Load(hulp.ExecuteReader());
-            LbAutos.ItemsSource = t.DefaultView;
-            MonteurVanAuto.Close();
+            DataTable? carsTable = db.GetCars();
+            if (carsTable == null)
+            {
+                MessageBox.Show("De auto's konden niet ingelezen worden. Waarschuw de service desk");
+                return;
+            }
+
+            lbAutos.ItemsSource = carsTable.DefaultView;
         }
 
         // Eventhandler zet de gegevens van de auto die in de ListBox is geselecteerd, in de controls 
         // die bedoeld zijn om te wijzigen. Is er geen auto geselecteerd, worden de controles leeggemaakt
         private void LbAutos_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (LbAutos.SelectedItem == null)
+            if (lbAutos.SelectedItem == null)
             {
+                tbJaarTeWijzigenAuto.Clear();
+                tbMerkTeWijzigenAuto.Clear();
+                imgAfbeeldingTeWijzigenAuto.Source = null;
                 return;
             }
 
-            DataRowView auto = (DataRowView)LbAutos.SelectedItem;
+            DataRowView auto = (DataRowView)lbAutos.SelectedItem;
 
-            tbauto.Text = auto["make"].ToString();
-            TbJaar.Text = auto["yearOfIntroduction"].ToString();
+            tbMerkTeWijzigenAuto.Text = auto["make"].ToString();
+            tbJaarTeWijzigenAuto.Text = auto["yearOfIntroduction"].ToString();
 
             if (auto["picture"] == DBNull.Value)
             {
-                wijzig = null;
-                imgAfbeelding1.Source = null;
+                afbeeldingBestaandeAuto = null;
+                imgAfbeeldingTeWijzigenAuto.Source = null;
             }
             else
             {
-                wijzig = (byte[])auto["picture"];
-                imgAfbeelding1.Source = new ImageSourceConverter().ConvertFrom(wijzig) as ImageSource;
+                afbeeldingBestaandeAuto = (byte[])auto["picture"];
+                imgAfbeeldingTeWijzigenAuto.Source
+                    = new ImageSourceConverter().ConvertFrom(afbeeldingBestaandeAuto) as ImageSource;
             }
         }
 
-
-        // Eventhandeler voegt een nieuwe auto toe aan de database
-        private void bewaar_Click(object sender, RoutedEventArgs e)
+        // Method controleert de door de gebruiker ingevulde gegevens
+        private bool ValidateCarValues(string merk, string jaar, out int introductiejaar)
         {
-            MonteurVanAuto.Open();
-            MySqlCommand sql = MonteurVanAuto.CreateCommand();
-            sql.CommandText =
-                $"INSERT INTO cars(carId,  make,  picture, yearOfIntroduction) " +
-                $"         VALUES ( null, @make, @picture, {TbJaar1.Text});";
-            sql.Parameters.AddRange(
-                new[]   {
-                    new MySqlParameter(){ ParameterName = "@make", Value = TbMerk2.Text},
-                    new MySqlParameter(){ ParameterName = "@picture", Value = nieuw, }
-                    }
-                );
-            sql.ExecuteReader();
-            MonteurVanAuto.Close();
+            introductiejaar = 0;
+            bool waardenZijnGoed = false;
 
-            lezen();
-        }
-
-        private void ok1_Click(object sender, RoutedEventArgs e)
-        {
-            if (LbAutos.SelectedItem == null)
-            {
-                MessageBox.Show("U heeft een auto geselecteerd");
-                return;
-            }
-            if (string.IsNullOrEmpty(tbauto.Text))
+            if (string.IsNullOrEmpty(merk))
             {
                 MessageBox.Show("Vul het merk van de auto in.");
-                return;
             }
-            if (string.IsNullOrEmpty(TbJaar.Text))
+            else if (string.IsNullOrEmpty(jaar))
             {
                 MessageBox.Show("Vul het introductiejaar van de auto in.");
-                return;
             }
-
-            if (!int.TryParse(TbJaar.Text, out int jaar) || jaar < 1769)
+            else if (!int.TryParse(jaar, out introductiejaar) || introductiejaar < 1769)
             {
                 MessageBox.Show("Het introductiejaar mag alleen uit cijfers bestaan en mag niet voor 1769 liggen.");
-                return;
             }
+            else
+            {
+                waardenZijnGoed = true;
+            }
+            return waardenZijnGoed;
+        }
 
-            try
+        // Eventhandeler voegt een nieuwe auto toe aan de database
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateCarValues(tbMerkNieuweAuto.Text, tbJaarNieuweAuto.Text, out int introductiejaar))
             {
-                DataTable t = new();
-                MonteurVanAuto.Open();
-                MySqlCommand hulp = MonteurVanAuto.CreateCommand();
-                hulp.CommandText =
-                    $"UPDATE cars SET make= '{tbauto.Text}',picture = @picture, yearOfIntroduction = {TbJaar.Text} " +
-                    $" WHERE carId = {LbAutos.SelectedValue};";
-                hulp.Parameters.AddWithValue("@picture", wijzig);
-                t.Load(hulp.ExecuteReader());
-                LbAutos.ItemsSource = t.DefaultView;
-                MonteurVanAuto.Close();
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show("Er is iets fout gegaan");
-            }
-            finally
-            {
-                new MainWindow().Show();
-                Close();
+                if (db.CreateCar(tbMerkNieuweAuto.Text, afbeeldingNieuweAuto, introductiejaar))
+                {
+                    PopulateLbCars();
+                    tbMerkNieuweAuto.Clear();
+                    tbJaarNieuweAuto.Clear();
+                    imgAfbeeldingNieuweAuto.Source = null;
+                }
+                else
+                {
+                    MessageBox.Show("De nieuwe auto kon niet worden toegevoegd. Neem contact op met de service desk");
+                }
             }
         }
 
-        private void nok_Click(object sender, RoutedEventArgs e)
+        // Eventhandeler wijzigt de gegevens van de geselecteerde auto in de database
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
         {
-            if (LbAutos.SelectedItem == null)
+            if (lbAutos.SelectedItem == null)
+            {
+                MessageBox.Show("Selecteer eerst de auto waarvan U de gegevens wilt wijzigen.");
+                return;
+            }
+            if (ValidateCarValues(tbMerkTeWijzigenAuto.Text, tbJaarTeWijzigenAuto.Text, out int introductiejaar))
+            {
+                if (db.UpdateCar((int)lbAutos.SelectedValue, tbMerkTeWijzigenAuto.Text, afbeeldingBestaandeAuto, introductiejaar))
+                {
+                    PopulateLbCars();
+                }
+                else
+                {
+                    MessageBox.Show("De gegevens van de auto konden niet worden gewijzigd. Neem contact op met de service desk.");
+                }
+            }
+        }
+
+        // Eventhandeler verwijdert de gegevens van de geselecteerde auto uit de database
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbAutos.SelectedItem == null)
             {
                 MessageBox.Show("Selecteer eerst de auto die u wilt verwijderen.");
                 return;
             }
 
-            try
+            if (db.DeleteCar((int)lbAutos.SelectedValue))
             {
-                MonteurVanAuto.Open();
-                MySqlCommand hulp = MonteurVanAuto.CreateCommand();
-                hulp.CommandText = $"DELETE FROM cars WHERE carId = " + LbAutos.SelectedValue;
-                hulp.ExecuteNonQuery();
-                MonteurVanAuto.Close();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
-            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
-        }
-
-        private void BtnAfbeelding1_Click(object sender, RoutedEventArgs e)
-        {
-            wijzig = GetLocalPicture();
-            if (wijzig != null)
-            {
-                imgAfbeelding1.Source =
-                    new ImageSourceConverter().ConvertFrom(wijzig) as ImageSource;
+                PopulateLbCars();
             }
             else
             {
-                imgAfbeelding1.Source = null;
+                MessageBox.Show("De gegevens van de auto konden niet worden verwijderd. Neem contact op met de service desk.");
             }
         }
 
-        private void BtnAfbeelding2_Click(object sender, RoutedEventArgs e)
+        // Method laat de gebruiker een afbeelding kiezen voor de te wijzigen auto. 
+        private void BtnAfbeeldingTeWijzigenAuto_Click(object sender, RoutedEventArgs e)
         {
-            nieuw = GetLocalPicture();
-            if (nieuw != null)
+            afbeeldingBestaandeAuto = GetLocalPicture();
+            if (afbeeldingBestaandeAuto != null)
             {
-                imgAfbeelding2.Source =
-                    new ImageSourceConverter().ConvertFrom(nieuw) as ImageSource;
+                imgAfbeeldingTeWijzigenAuto.Source =
+                    new ImageSourceConverter().ConvertFrom(afbeeldingBestaandeAuto) as ImageSource;
             }
             else
             {
-                imgAfbeelding2.Source = null;
+                imgAfbeeldingTeWijzigenAuto.Source = null;
+            }
+        }
+
+        // Method laat de gebruiker een afbeelding kiezen voor de nieuw toe te voegen auto. 
+        private void BtnAfbeeldingNieuweAuto_Click(object sender, RoutedEventArgs e)
+        {
+            afbeeldingNieuweAuto = GetLocalPicture();
+            if (afbeeldingNieuweAuto != null)
+            {
+                imgAfbeeldingNieuweAuto.Source =
+                    new ImageSourceConverter().ConvertFrom(afbeeldingNieuweAuto) as ImageSource;
+            }
+            else
+            {
+                imgAfbeeldingNieuweAuto.Source = null;
             }
         }
 
---
+++ Lj2Dd1En2/Models/CarsDb.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace Lj2Dd1En2.Models
+{
+    public class CarsDb
+    {
+        MySqlConnection conn = new MySqlConnection("server=localhost;database=carsdb;uid=root;pwd=;");
+
+        // Method GetCars leest alle autos uit de databasetabel cars in en zet deze in een DataTable.
+        // De waarde van GetCars is:
+        // - null: Er is trad een onverwachte fout (esception) op.
+        // - !null: De DataTable, gevuld met gegevens over de autos.
+        public DataTable? GetCars()
+        {
+            DataTable? carTable = new DataTable();
+            try
+            {
+                conn.Open();
+                MySqlCommand sql = conn.CreateCommand();
+                sql.CommandText =
+                    @"
+                        SELECT c.carId, c.make, c.picture, c.yearOfIntroduction
+                        FROM cars c                    
+                    ";
+                MySqlDataReader carReader = sql.ExecuteReader();
+                carTable.Load(carReader);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                carTable = null;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return carTable;
+        }
+
+        // Method GetCar leest uit de database tabel cars, de gegevens in van ed auto met de opgegeven id
+        // De waarde van GetCar is:
+        // - null: Er is trad een onverwachte fout (esception) op.
+        // - !null: De DataTable, gevuld met gegevens over de auto
+        // NB: werd de auto niet gevonden, dan is de DataTable leeg
+        public DataTable? GetCar(int id)
+        {
+            DataTable? carTable = new DataTable();
+            try
+            {
+                conn.Open();
+                MySqlCommand sql = conn.CreateCommand();
+                sql.CommandText =
+                    @"
+                        SELECT c.carId, c.make, c.picture, c.yearOfIntroduction
+                        FROM cars c
+                        WHERE carId = @carId;
+                    ";
+                sql.Parameters.AddWithValue("@carId", id);
+                MySqlDataReader carReader = sql.ExecuteReader();
+                carTable.Load(carReader);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                carTable = null;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return carTable;
+        }
+
+        // Method CreateCar voegt de gegevens van een auto toe aan de database.
+        // De waarde van CreateCar is:
+        // - true: de gegevens zijn toegevoegd
+        // - false: de gegevens zijn niet toegevoegd, mogelijk door een exception
+        public bool CreateCar(string make, byte[]? picture, int yearOfIntroduction)
+        {
+            bool toevoegenGelukt = false;
+            try
+            {
+                conn.Open();
+                MySqlCommand sql = conn.CreateCommand();
+                sql.CommandText =
+                    @"
+                        INSERT INTO cars (carId, make, picture, yearOfIntroduction) 
+                        VALUES (NULL, @make, @picture, @yearOfIntroduction)
+                    ";
+                sql.Parameters.AddWithValue("@make", make);
+                sql.Parameters.AddWithValue("@picture", picture);
+                sql.Parameters.AddWithValue("@yearOfIntroduction", yearOfIntroduction);
+
+                int rowsAffected = sql.ExecuteNonQuery();
+
+                if (rowsAffected == 1)
+                {
+                    toevoegenGelukt = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return toevoegenGelukt;
+        }
+
+        // Method UpdateCar wijzigt de databasegegevens van de auto met id carId.
+        // De waarde van UpdateCar:
+        // - true: wijzigen is goed gegaan
+        // - false: wijzigen is niet goed gegaan of een exception. 
+        public bool UpdateCar(int carId, string make, byte[]? picture, int yearOfIntroduction)
+        {
+            bool wijzigenGelukt;
+
+            try
+            {
+                conn.Open();
+                MySqlCommand sql = conn.CreateCommand();
+                sql.CommandText =
+                    @"
+                        UPDATE cars 
+                        SET make = @make, 
+                            picture = @picture,
+                            yearOfIntroduction = @yearOfIntroduction
+                        WHERE carId = @carId;
+                    ";
+                sql.Parameters.AddWithValue("@carId", carId);
+                sql.Parameters.AddWithValue("@make", make);
+                sql.Parameters.AddWithValue("@picture", picture);
+                sql.Parameters.AddWithValue("@yearOfIntroduction", yearOfIntroduction);
+
+                int rowsAffected = sql.ExecuteNonQuery();
+                if (rowsAffected == 1)
+                {
+                    wijzigenGelukt = true;
+                }
+                else
+                {
+                    wijzigenGelukt = false;
+                    Console.Error.WriteLine($"Auto met id {carId} niet in database aanwezig");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                wijzigenGelukt = false;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return wijzigenGelukt;
+        }
+
+        // Method DeleteCar verwijdert de databasegegevens van de auto met id carId.
+        // De waarde van DeleteCar :
+        // - true: verwijderen is goed gegaan
+        // - false: verwijderen is niet goed gegaan of een exception. 
+        public bool DeleteCar(int carId)
+        {
+            bool verwijderenGelukt;
+
+            try
+            {
+                conn.Open();
+                MySqlCommand sql = conn.CreateCommand();
+                sql.CommandText =
+                    @"
+                        DELETE FROM cars 
+                        WHERE carId = @carId;
+                    ";
+                sql.Parameters.AddWithValue("@carId", carId);
+
+                if (sql.ExecuteNonQuery() == 1)
+                {
+                    verwijderenGelukt = true;
+                }
+                else
+                {
+                    verwijderenGelukt = false;
+                    Console.Error.WriteLine($"Auto met id {carId} niet in database aanwezig");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                verwijderenGelukt = false;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return verwijderenGelukt;
+        }
+    }
+}
